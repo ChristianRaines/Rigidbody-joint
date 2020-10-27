86846b9aeca31c099a46ec3ee2a9c4ef0e937fe7
网络巨人 <982801481@qq.com>
2020-10-27 18:55:11 +0800

    homework

---
+++ RigidBody&Joint/Assets/force.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class force : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody mybody;
+    void Start()
+    {
+        mybody = gameObject.GetComponent<Rigidbody>();
+        mybody.AddForce(new Vector3(0, 0, 50), ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
